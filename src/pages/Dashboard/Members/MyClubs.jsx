b17a2bb23a93b75b07159fc32c494e1d18bf8268
abdurrazzaqe <arazzak714@gmail.com>
2025-12-16 04:31:14 +0600

    member clubs & event component added

---
+++ src/pages/Dashboard/Members/MyClubs.jsx
@@ -0,0 +1,41 @@
+import React, { useContext } from "react";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { AuthContext } from "../../../contexts/AuthContext/AuthContext";
+import { useQuery } from "@tanstack/react-query";
+import { Link } from "react-router";
+
+const MyClubs = () => {
+  const { user } = useContext(AuthContext);
+  const axiosSecure = useAxiosSecure();
+
+  const { data: Memberships = [] } = useQuery({
+    queryKey: ["my-clubs", user?.email],
+    enabled: !!user?.email,
+    queryFn: async () => {
+      const res = await axiosSecure.get(
+        `/event-registrations/user/${user.email}`
+      );
+      return res.data.filter((m) => m.status === "resigtered");
+    },
+  });
+  return (
+    <div className="grid md:grid-cols-3 gap-6">
+      {Memberships.map((m) => (
+        <div key={m._id} className="p-4 bg-white rounded shadow">
+          <h2 className="text-xl font-bold">{m.clubName}</h2>
+          <p>Location: {m.location}</p>
+          <p>Status: {m.status}</p>
+          <p>Expiry: {new Date(m.expiryDate).toLocaleDateString()}</p>
+          <Link
+            to={`/clubs/${m.clubId}`}
+            className="text-teal-500 mt-2 inline-block"
+          >
+            View Details
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default MyClubs;
---
+++ src/pages/Dashboard/Members/MyEvents.jsx
@@ -0,0 +1,49 @@
+import React, { useContext } from "react";
+import { AuthContext } from "../../../contexts/AuthContext/AuthContext";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+
+const MyEvents = () => {
+  const { user } = useContext(AuthContext);
+  const axiosSecure = useAxiosSecure();
+
+  const { data: events = [] } = useQuery({
+    queryKey: ["my-events", user?.email],
+    enabled: !!user?.email,
+    queryFn: async () => {
+      const res = await axiosSecure.get(
+        `/event-registrations/user/${user.email}`
+      );
+      return res.data;
+    },
+  });
+  return (
+    <div className="overflow-x-auto">
+      <table className="table table-zebra">
+        {/* head */}
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Event</th>
+            <th>Club</th>
+            <th>Date</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {events.map((ev, index) => (
+            <tr key={ev._id}>
+              <th>{index + 1}</th>
+              <td>{ev.eventTitle}</td>
+              <td>{ev.clubName}</td>
+              <td>{new Date(ev.eventDate).toLocaleString()}</td>
+              <td>{ev.status}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default MyEvents;
